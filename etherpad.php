ea3e93a6bf8999a72f7b5068d9a1020f094aa516
Boone B Gorges <boonebgorges@gmail.com>
2012-09-25 11:54:33 -0400

    Cleanup in main plugin file

--- etherpad.php
+++ etherpad.php
@@ -32,42 +32,9 @@ function wp_etherpad_bootstrap() {
 add_action( 'init', 'wp_etherpad_bootstrap' );
 
 
-######################################
-################## ACTIONS HOOKS
-######################################
-
-
-/**
- * Applied to the HTML DIV created to house the rich text editor, prior to printing it on the screen. Filter function argument/return value is a string.
- */
-
-/**
- * Applied to post content before putting it into a rich editor window.
- */
-add_action('the_editor_content', 'etherpad_editor_content');
-add_action('admin_menu', 'etherpad_register_admin_page');
-
-
-/**
- * Runs just before the "advanced" section of the post editing form in the admin menus.
- */
-#add_action('edit_form_advanced', 'etherpad_edit_form_advanced');
-
-
-
-function etherpad_editor_content($content){
-}
-
-function etherpad_edit_form_advanced($content){
-  echo $content;
-}
-
-
 /**
  * Adds the etherpad under the Settings in WPAdmin
  */
-
-
 function etherpad_register_admin_page() {
 	add_submenu_page( 'options-general.php', 'Etherpad', 'Etherpad Settings', 'manage_options', 'my-custom-submenu-page', 'etherpad_admin_page' );
 }
